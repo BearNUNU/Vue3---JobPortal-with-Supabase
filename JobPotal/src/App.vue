304514a2a6c9b1d4493481e41c6e6206e8b7785e
GunWoo <kkw31025@gmail.com>
2024-12-16 15:49:49 +0900

    [build] - Project setting

---
+++ JobPotal/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Vue3 job</h1>
+</template>
+
+<style scoped lang="scss">
+div {
+  h1 {
+    color: red;
+    &:hover{
+      color: blue;
+    }
+  }
+}
+</style>
