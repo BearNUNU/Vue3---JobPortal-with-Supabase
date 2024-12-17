03c033a2cf60ed65d04a1e0fae4ac091dc3d341b
GunWoo <kkw31025@gmail.com>
2024-12-17 12:55:33 +0900

    [stlye] - Change from scope css to form.scss

--- JobPotal/src/App.vue
+++ JobPotal/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <router-view/>
+  <div>
+    <Navbar/>
+    <router-view/>
+  </div>
 </template>
 
 <script setup>
+import Navbar from './components/Navbar.vue';
 </script>
 
 
 <style scoped lang="scss">
 </style>
- 
+  
