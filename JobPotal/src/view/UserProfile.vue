3bd093339f04893daa0662bf3331d1317b749b03
GunWoo <kkw31025@gmail.com>
2024-12-20 15:57:03 +0900

    [feat] - Add apply_list, apllicant_list

--- JobPotal/src/view/UserProfile.vue
+++ JobPotal/src/view/UserProfile.vue
@@ -12,6 +12,20 @@
       <p>{{ introduce }}</p>
     </div>
       <button class="logout" @click="handleLogout">로그아웃</button>
+      <details class="job-list" >
+        <summary>내 지원내역</summary>
+        <p v-for="job in job_apply_list" :key="job.id">
+          <span>[지원완료] {{  job.job_title }}</span>
+          <time>{{ new Date(job.created_at).toLocaleDateString() }}</time>
+        </p>
+      </details>
+      <details class="job-list" >
+        <summary>받은 지원내역</summary>
+        <p v-for="job in job_recieve_list" :key="job.id">
+          <span>{{ job.applicant_name }}님이 <q>{{ job.job_title }}</q>에 지원했습니다.</span>
+          <time>{{ new Date(job.created_at).toLocaleDateString() }}</time>
+        </p>
+      </details>
     </div>
   </template>
   
@@ -19,13 +33,44 @@
   import supabase from '../supabase';
   import { onMounted, ref } from 'vue';
   import { useRouter } from 'vue-router';
+  import {useAuth} from "../auth/auth.js";
 
+
+  const { isLogin, user, checkLoginStatus } = useAuth();
   const router = useRouter();
-  const isLogin = ref(false)
   const name = ref('');
   const address = ref('');
   const introduce = ref('')
+  const job_apply_list = ref([]);
+  const job_recieve_list = ref([]);
 
+  const getRecieveList = async () => {
+    const { data, error } = await supabase
+        .from('job_apply_list')
+        .select()
+        .eq('employer_id', user.value.id)
+
+    if(error) {
+      alert('받은 지원내역 가져오기 실패');
+    } else {
+      job_recieve_list.value = data;
+      console.log('job_recieve_list:', job_recieve_list.value);
+    }
+  }
+
+  const getApplyList = async () => {
+    const { data, error } = await supabase
+        .from('job_apply_list')
+        .select()
+        .eq('applicant_id', user.value.id)
+
+    if(error) {
+      alert('지원내역 가져오기 실패');
+    } else {
+      job_apply_list.value = data;
+      console.log('', job_apply_list.value);
+    }
+  }
 
   const handleLogout = async () => {
   const { error } = await supabase.auth.signOut()
@@ -37,91 +82,110 @@
   }
 }
 
-  onMounted(async() =>{
-    const { data: { user } } = await supabase.auth.getUser()
-    if(user){
-      console.log(user.email);  
-      isLogin.value = true
-      const { data, error } = await supabase
-      .from('user_table')
-      .select()
-      .eq('id', user.id)
-    if(data) {
-      console.log('user data:', data);
-      name.value = data[0].name;
-      address.value = data[0].address;
-      introduce.value = data[0].introduce
-    }
+  onMounted(async() => {
+    await checkLoginStatus();
+    await getApplyList();
+    await getRecieveList();
 
-  } else {
-    console.log('로그아웃 상태')
-    isLogin.value = false
-    alert('로그인 후 이용해주세요.')
-    router.push('/')
-  }
+    const { data, error } = await supabase
+        .from('user_table')
+        .select()
+        .eq('id', user.value.id)
+        .single()
+    name.value = data.name;
+    address.value = data.address;
+    introduce.value = data.introduce;
   })
   </script>
-  
-  <style lang="scss" scoped>
- button { 
-    background: transparent;
-    color: var(--main-color);
-    font-size: 16px;
-    margin-top: 40px;
-    border : none;
 
-    &:hover{
-      opacity: 0.7;
-      text-decoration: underline;
-    }
-  }  
+<style lang="scss" scoped>
+// 버튼 디자인 수정
+button {
+  background: transparent;
+  color: var(--main-color);
+  font-size: 16px;
+  margin-top: 40px;
+  &:hover{
+    opacity: 0.7;
+    text-decoration: underline;
+  }
+}
 
-  .form-container {
-    margin-top: 20px;
+.form-container {
+  margin-top: 20px;
+}
+
+.top-info-box {
+  display: flex;
+  align-items: center;
+  margin-bottom: 40px;
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 12px;
   }
 
-  .top-info-box {
-    display: flex;
-    align-items: center;
-    margin-bottom: 40px;
+  .right-info {
+    width: 100%;
+    line-height: 1.4;
 
-    img {
-      width: 40px;
-      height: 40px;
-      border-radius: 50%;
-      margin-right: 12px;
+    .name {
+      font-size: 16px;
+      font-weight: 600;
     }
-
-    .right-info {
-      width: 100%;
-      line-height: 1.4;
-  
-      .name {
-        font-size: 16px;
-        font-weight: 600;
-      }
-      address {
-        font-size: 14px;
-        font-weight: 400;
-        font-style: normal;
-        color: #777;
-      }
-    }
-  }
-
-  .text-info {
-    margin-bottom: 25px;
-    h4 {
-      margin-bottom: 8px;
-    }
-    p {
+    address {
       font-size: 14px;
-      color: #333;
-      line-height: 20px;
-      border: 1px solid #d9d9d9;
-      border-radius: 8px;
-      padding: 12px 1rem;
+      font-weight: 400;
+      font-style: normal;
+      color: #777;
+    }
+  }
+}
+
+.text-info {
+  margin-bottom: 25px;
+  h4 {
+    margin-bottom: 8px;
+  }
+  p {
+    font-size: 14px;
+    color: #333;
+    line-height: 20px;
+    border: 1px solid #d9d9d9;
+    border-radius: 8px;
+    padding: 12px 1rem;
+  }
+}
+
+.job-list {
+  list-style-type: none;
+  font-size: 14px;
+  padding: 1rem 0;
+
+  summary {
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 10px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #d9d9d9;
+  }
+
+  p {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    time {
+      color: #777;
+      font-size: 12px;
+      font-weight: 400;
+      width: 10em;
+      // outline: 1px solid red;
+      text-align: right;
     }
   }
 
+  q { font-weight: bold;}
+}
 </style>
