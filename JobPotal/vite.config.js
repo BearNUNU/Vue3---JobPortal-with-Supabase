28c7e74c23b884938f248b4903e29b6e220784a7
GunWoo <kkw31025@gmail.com>
2024-12-24 14:19:57 +0900

    [build] - Add pwa build script

--- JobPotal/vite.config.js
+++ JobPotal/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { VitePWA} from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '구인구직',
+        short_name: '구인구직',
+        description: '쉽고 편리한 구인구직 서비스',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
